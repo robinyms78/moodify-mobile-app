0654fa5c35172d9fbd94b0ce4c54e871046c8246
fatboy13-Adrian <birdbird_dog@hotmail.sg>
2025-05-13 16:30:55 +0800

    Created basic login form for the project
    1. Created login form.
    2.  Installed required dependencies for the login form to work.
    3. Refactored App and Index files.
    4. Tested on both Android emulator and physical Iphone and confirmed working on both platforms.
    5. Uploaded Android emulator screenshot onto asset/images folder.

--- moodify-app/App.js
+++ moodify-app/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import LoginForm from './LoginForm';
 
-export default function App() {
+export default function App() 
+{
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
+      <LoginForm />
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
